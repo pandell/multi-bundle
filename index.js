a02d5ed4784f310f0f312bb8521b462c41e6db87
John Gozde <johng@pandell.com>
2014-07-21 18:53:01 -0600

    Add tests & fix implementation for multi-bundle output.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 "use strict";
 
 var path = require("path");
-var util = require("util");
 
 var clone = require("clone");
 var isPlainObject = require("lodash.isplainobject");
@@ -15,19 +14,7 @@ var stream = require("readable-stream");
 var gce = require("./lib/gce");
 var sort = require("./lib/toposort");
 
-/*
-{
-    common: {
-        start: "start.js",
-        group: {
-            stop: "stop.js",
-            pause: ["pause.js", "resume.js"]
-        }
-    },
-    oneOff: "oneoff.js"
-}
-*/
-
+//-----------------------------------------------
 function depsStream(deps) {
     var s = new stream.PassThrough({ objectMode: true });
     Rx.Node.writeToStream(deps, s);
@@ -37,16 +24,17 @@ function depsStream(deps) {
     };
 }
 
+//-----------------------------------------------
 function buildEntryCompiler(name, deps, opts, cb) {
     var ourDeps = deps
-        .filter(R.compose(R.contains(name), R.prop("level")))
+        .filter(R.compose(R.eq(name), R.last, R.prop("level")))
+        .distinct(R.prop("id"))
         .share();
 
     var compiler = opts.browserify(R.mixin(opts, { deps: depsStream(ourDeps) }));
 
     return ourDeps.distinct(R.prop("id")).subscribe(
         function onNext(row) {
-            console.log("row: ", util.inspect(row));
             if (row.external) {
                 compiler.external(row.id);
             } else if (row.entry) {
@@ -60,17 +48,14 @@ function buildEntryCompiler(name, deps, opts, cb) {
     );
 }
 
+//-----------------------------------------------
 function buildCommonCompiler(name, deps, opts, cb) {
 
     function exceedsThreshold(ctx, row) {
         return (ctx.rows[row.id].length > opts.threshold && gce(R.pluck("level", ctx.rows[row.id])) === name) || ctx.ensureCommon[row.id];
     }
 
-    var ourDeps = deps
-        .filter(R.compose(R.contains(name), R.prop("level")))
-        .share();
-
-    return ourDeps.aggregate({ rows: {}, ensureCommon: {} }, function (ctx, row) {
+    var commonDeps = deps.filter(R.compose(R.contains(name), R.prop("level"))).aggregate({ rows: {}, ensureCommon: {} }, function (ctx, row) {
         if (!ctx.rows[row.id]) {
             ctx.rows[row.id] = [];
         }
@@ -83,59 +68,72 @@ function buildCommonCompiler(name, deps, opts, cb) {
     }).map(function (ctx) {
         var rowValues = R.flatten(R.values(ctx.rows));
         var externals = R.filter(R.prop("external"), rowValues);
-        var requires = R.filter(R.curry(exceedsThreshold)(ctx), rowValues);
-        var childDeps = R.differenceWith(R.compose(R.eq, R.prop("id")), rowValues, R.union(externals, requires));
+        var requires = R.filter(R.curry(exceedsThreshold)(ctx), R.reject(R.prop("external"), rowValues));
+        var childDeps = R.filter(function (row) {
+            return !R.containsWith(R.eqProps("id"), row, externals) && !R.containsWith(R.eqProps("id"), row, requires);
+        }, rowValues);
 
         return { externals: externals, requires: requires, childDeps: childDeps };
-    }).do(function (res) {
-        var compiler = opts.browserify(R.mixin(opts, { deps: depsStream(ourDeps) }));
+    }).single().share();
 
-        R.each(compiler.external, R.uniq(R.pluck("id", res.externals)));
-        R.each(compiler.require, R.uniq(R.pluck("id", res.requires)));
+    commonDeps.subscribe(
+        function onNext(res) {
+            var compiler = opts.browserify(R.mixin(opts, {
+                deps: depsStream(Rx.Observable.fromArray([].concat(res.externals, res.requires)).distinct(R.prop("id")))
+            }));
 
-        cb(null, { name: name, compiler: compiler });
-    }).flatMap(function (res) {
-        return sort(Rx.Observable.fromArray([].concat(
+            R.each(compiler.external.bind(compiler), R.uniq(R.pluck("id", res.externals)));
+            R.each(compiler.require.bind(compiler), R.uniq(R.pluck("id", res.requires)));
+
+            cb(null, { name: name, compiler: compiler });
+        },
+        cb, // onError
+        null // onComplete
+    );
+
+    return commonDeps.flatMap(function (res) {
+        return Rx.Observable.fromArray([].concat(
             res.externals,
             R.map(R.mixin({ external: true }), res.requires),
             res.childDeps
-        )));
-    }).catch(cb);
+        ));
+    });
 }
 
+//-----------------------------------------------
 function buildCompilers(config, deps, opts, cb) {
     R.each(function (name) {
         var entry = config[name];
         if (isPlainObject(entry)) {
-            console.log("building common compiler");
             var childDeps = buildCommonCompiler(name, deps, opts, cb);
             buildCompilers(entry, childDeps, opts, cb);
         } else {
-            console.log("building entry compiler", name, opts);
             buildEntryCompiler(name, deps, opts, cb);
         }
     }, R.keys(config));
 }
 
+//-----------------------------------------------
 function concatDeps(config, opts, level) {
     return Rx.Observable.concat(R.map(function (name) {
         var entry = config[name];
         if (isPlainObject(entry)) {
             return Rx.Observable.concat(concatDeps(entry, opts, level.concat(name)));
         }
-        var fullPath = path.resolve(opts.basedir || process.cwd(), entry);
-        var deps = mdeps([].concat(fullPath), opts);
-        return Rx.Node.fromStream(deps).map(R.mixin({ level: level.concat(name) }));
+        var fullPaths = R.map(R.applyLeft(path.resolve, opts.basedir || process.cwd()), [].concat(entry));
+        var deps = mdeps(fullPaths, opts);
+        return sort(Rx.Node.fromStream(deps)).map(R.mixin({ level: level.concat(name) }));
     }, R.keys(config)));
 }
 
+//-----------------------------------------------
 module.exports = function createMultiBundle(entryConfig, opts, cb) {
     var config = {};
 
     if (typeof entryConfig === "string") {
         config[path.basename(entryConfig, path.extname(entryConfig))] = entryConfig;
     } else if (Array.isArray(entryConfig)) {
-        config.common = R.clone(entryConfig);
+        config.bundle = R.clone(entryConfig);
     } else if (isPlainObject(entryConfig)) {
         config = clone(entryConfig);
     } else {
@@ -146,6 +144,6 @@ module.exports = function createMultiBundle(entryConfig, opts, cb) {
         opts.threshold = 1;
     }
 
-    var deps = sort(concatDeps(config, opts, []));
+    var deps = concatDeps(config, opts, []);
     buildCompilers(config, deps, opts, cb);
 };
--- tests/multibundle.tests.js
+++ tests/multibundle.tests.js
@@ -10,6 +10,7 @@ var R = require("ramda");
 
 var bundle = require("../");
 
+//-----------------------------------------------
 function BrowserifyMock(opts) {
     if (!(this instanceof BrowserifyMock)) { return new BrowserifyMock(opts); }
 
@@ -31,14 +32,18 @@ BrowserifyMock.prototype.external = function (file) {
     this.externals.push(file);
 };
 
-var resolve = R.map(path.resolve);
+//-----------------------------------------------
+var resolve = R.map(R.compose(path.resolve, function (p) { return __dirname + "/fixtures/" + p; }));
 
+//-----------------------------------------------
 test("multi-bundle", function (t) {
 
+    //-------------------------------------------
     t.test("for a single entry point", function (t) {
 
         var opts = { browserify: BrowserifyMock, basedir: __dirname };
 
+        //---------------------------------------
         t.test("produces a single bundle", function (t) {
 
             bundle("./fixtures/oneoff.js", opts, function (err, res) {
@@ -50,16 +55,19 @@ test("multi-bundle", function (t) {
 
         });
 
-        t.test("specifies dependencies correctly", function (t) {
+        //---------------------------------------
+        t.test("includes entry modules with no externals", function (t) {
 
             bundle("./fixtures/oneoff.js", opts, function (err, res) {
                 t.error(err, "no errors");
-                t.deepEqual(res.compiler.files, resolve([__dirname + "/fixtures/oneoff.js"]), "only entry module is added");
+                t.deepEqual(res.compiler.files, resolve(["oneoff.js"]), "only entry module is added");
+                t.deepEqual(res.compiler.externals, [], "no externals are added");
                 t.end();
             });
 
         });
 
+        //---------------------------------------
         t.test("passes dependency stream to compiler options", function (t) {
 
             bundle("./fixtures/oneoff.js", opts, function (err, res) {
@@ -69,14 +77,181 @@ test("multi-bundle", function (t) {
                 t.equal(typeof res.compiler.opts.deps, "function", "deps is a function");
 
                 res.compiler.opts.deps().pipe(concat(function (deps) {
-                    t.deepEqual(R.pluck("id", deps), resolve([
-                        __dirname + "/fixtures/z.js",
-                        __dirname + "/fixtures/a.js",
-                        __dirname + "/fixtures/d.js",
-                        __dirname + "/fixtures/oneoff.js"
-                    ]), "all deps are included");
-
+                    t.deepEqual(
+                        R.pluck("id", deps),
+                        resolve(["z.js", "a.js", "d.js", "oneoff.js"]),
+                        "all deps are included"
+                    );
                     t.end();
+                }));
+
+            });
+
+        });
+
+    });
+
+    //-------------------------------------------
+    t.test("for an array entry point", function (t) {
+
+        var opts = { browserify: BrowserifyMock, basedir: __dirname };
+
+        //---------------------------------------
+        t.test("produces a single bundle", function (t) {
+
+            bundle(["./fixtures/a.js", "./fixtures/b.js"], opts, function (err, res) {
+                t.error(err, "no errors");
+                t.equal(res.name, "bundle", "name is 'bundle'");
+                t.ok(res.compiler instanceof BrowserifyMock, "compiler is mocked instance");
+                t.end();
+            });
+
+        });
+
+        //---------------------------------------
+        t.test("includes entry modules with no externals", function (t) {
+
+            bundle(["./fixtures/a.js", "./fixtures/b.js"], opts, function (err, res) {
+                t.error(err, "no errors");
+                t.deepEqual(res.compiler.files, resolve(["a.js", "b.js"]), "only entry modules are added");
+                t.deepEqual(res.compiler.externals, [], "no externals are added");
+                t.end();
+            });
+
+        });
+
+        //---------------------------------------
+        t.test("passes dependency stream to compiler options", function (t) {
+
+            bundle(["./fixtures/a.js", "./fixtures/b.js"], opts, function (err, res) {
+                t.error(err, "no errors");
+
+                t.ok(res.compiler.opts.deps, "deps was specified");
+                t.equal(typeof res.compiler.opts.deps, "function", "deps is a function");
+
+                res.compiler.opts.deps().pipe(concat(function (deps) {
+                    t.deepEqual(
+                        R.pluck("id", deps),
+                        resolve(["z.js", "a.js", "y.js", "b.js"]),
+                        "all deps are included"
+                    );
+                    t.end();
+                }));
+
+            });
+
+        });
+
+    });
+
+    //-------------------------------------------
+    t.test("for a nested set of entry points", function (t) {
+
+        var opts = { browserify: BrowserifyMock, basedir: __dirname };
+        var entryConfig = {
+            common: {
+                start: "./fixtures/start.js",
+                group: {
+                    stop: "./fixtures/stop.js",
+                    pause: ["./fixtures/pause.js", "./fixtures/resume.js"]
+                }
+            },
+            oneoff: "./fixtures/oneoff.js"
+        };
+
+        //---------------------------------------
+        t.test("produces common and entry bundles", function (t) {
+            // depth-first traversal
+            var names = ["common", "start", "group", "stop", "pause", "oneoff"];
+
+            t.plan(names.length * 4); // 4 assertions per bundle
+
+            var b = 0;
+            bundle(entryConfig, opts, function (err, res) {
+                t.error(err, "no errors");
+                t.ok(res, "has result");
+                t.equal(res.name, names[b], "name is '" + names[b] + "'");
+                t.ok(res.compiler instanceof BrowserifyMock, "compiler is mocked instance");
+                b += 1;
+            });
+        });
+
+        //---------------------------------------
+        t.test("includes entry modules with correct externals", function (t) {
+            var expected = {
+                "start": {
+                    files: resolve(["start.js"]),
+                    externals: resolve(["z.js", "a.js"])
+                },
+                "stop": {
+                    files: resolve(["stop.js"]),
+                    externals: resolve(["z.js", "a.js", "y.js", "b.js"])
+                },
+                "pause": {
+                    files: resolve(["pause.js", "resume.js"]),
+                    externals: resolve(["z.js", "a.js", "y.js", "b.js"])
+                },
+                "oneoff": {
+                    files: resolve(["oneoff.js"]),
+                    externals: []
+                }
+            };
+
+            t.plan(12); // 4 entries * 3 assertions
+
+            bundle(entryConfig, opts, function (err, res) {
+                if (!expected[res.name]) { return; }
+                t.error(err, "no errors");
+                t.deepEqual(res.compiler.files, expected[res.name].files, res.name + ": only entry modules are added");
+                t.deepEqual(res.compiler.externals, expected[res.name].externals, res.name + ": external dependencies are marked");
+            });
+        });
+
+        //---------------------------------------
+        t.test("requires common dependencies with correct externals", function (t) {
+            var expected = {
+                "common": {
+                    requires: resolve(["z.js", "a.js"]),
+                    externals: []
+                },
+                "group": {
+                    requires: resolve(["y.js", "b.js"]),
+                    externals: resolve(["z.js", "a.js"])
+                }
+            };
+
+            t.plan(8); // 2 modules * 4 assertions
+
+            bundle(entryConfig, opts, function (err, res) {
+                if (!expected[res.name]) { return; }
+                t.error(err, "no errors");
+                t.deepEqual(res.compiler.files, [], res.name + ": no entry modules are added");
+                t.deepEqual(res.compiler.requires, expected[res.name].requires, res.name + ": required dependencies are added");
+                t.deepEqual(res.compiler.externals, expected[res.name].externals, res.name + ": external dependencies are marked");
+            });
+        });
+
+        //---------------------------------------
+        t.test("passes dependency stream to compiler options", function (t) {
+            var expected = {
+                "common": resolve(["z.js", "a.js"]),
+                "group": resolve(["z.js", "a.js", "y.js", "b.js"]),
+                "start": resolve(["z.js", "a.js", "d.js", "start.js"]),
+                "stop": resolve(["z.js", "a.js", "y.js", "b.js", "stop.js"]),
+                "pause": resolve(["z.js", "a.js", "y.js", "b.js", "x.js", "c.js", "pause.js", "resume.js"]),
+                "oneoff": resolve(["z.js", "a.js", "d.js", "oneoff.js"])
+            };
+
+            t.plan(24); // 6 bundles * 4 assertions
+
+            bundle(entryConfig, opts, function (err, res) {
+                t.error(err, "no errors");
+
+                t.ok(res.compiler.opts.deps, "deps was specified");
+                t.equal(typeof res.compiler.opts.deps, "function", "deps is a function");
+
+                res.compiler.opts.deps().pipe(concat(function (deps) {
+                    t.deepEqual(R.pluck("id", deps), expected[res.name], res.name + ": all deps are included");
                 }));
 
             });
