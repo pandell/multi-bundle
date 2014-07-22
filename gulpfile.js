b5c67fec2f74c356f5ea98a1d6b4f25601e4084b
John Gozde <john@gozde.ca>
2014-07-22 00:34:42 -0600

    Add example usage to gulpfile using browserify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-/*jslint node: true, vars: true */
+/*jslint node: true, unparam: true, vars: true */
 
 "use strict";
 
@@ -7,10 +7,12 @@ var jshint = require("gulp-jshint");
 var jslint = require("gulp-jslint-simple");
 var mocha = require("gulp-mocha");
 var monitorCtrlC = require("monitorctrlc");
+var rimraf = require("gulp-rimraf");
 var taskFromStreams = require("gulp-taskfromstreams");
 
 var srcFiles = "{.,./lib}/*.js*";
 var testFiles = "tests/*.js";
+var exampleOut = "./build";
 
 gulp.task("lint", taskFromStreams(function () {
     return [
@@ -23,8 +25,40 @@ gulp.task("lint", taskFromStreams(function () {
 
 gulp.task("test", ["lint"], taskFromStreams(function () {
     return [
-        gulp.src(testFiles),
+        gulp.src(testFiles, { read: false }),
         mocha({ reporter: "spec" })
+    ];
+}));
+
+gulp.task("clean", taskFromStreams(function () {
+    return [
+        gulp.src(exampleOut, { read: false }),
+        rimraf()
+    ];
+}));
+
+gulp.task("example", ["lint", "test", "clean"], taskFromStreams(function () {
+    var browserify = require("browserify");
+    var source = require("vinyl-source-stream");
+
+    var multi = require("./index");
+
+    var entryConfig = {
+        common: {
+            start: "./tests/fixtures/start.js",
+            group: {
+                stop: "./tests/fixtures/stop.js",
+                pause: ["./tests/fixtures/pause.js", "./tests/fixtures/resume.js"]
+            }
+        },
+        oneoff: "./tests/fixtures/oneoff.js"
+    };
+
+    var m = multi(entryConfig, { browserify: browserify });
+
+    return [
+        m.bundle({ objectMode: true, debug: true }, function (name) { return source(name + ".js"); }),
+        gulp.dest(exampleOut)
     ];
 }));
 
